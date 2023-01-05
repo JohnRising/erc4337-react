999863a81f11c44a2157e6833155811b0f6637fc
Alexis Davidson <alexisdavidson00@gmail.com>
2023-01-05 12:06:37 +0700

    Import ethers and erc-4337-examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from "ethers";
+import {getSimpleAccount} from "erc-4337-examples/src";
+
+const config = {
+  "bundlerUrl": "http://localhost:4337",
+  "rpcUrl": "https://rpc-mumbai.maticvigil.com/",
+  "signingKey": "0x094ddde528fa30e0ab36eaabc6256a7bc78fb2953d21d31447b9577f855e1795",
+  "entryPoint": "0x78d4f01f56b982a3B03C4E127A5D3aFa8EBee686",
+  "simpleAccountFactory": "0xe19E9755942BB0bD0cCCCe25B1742596b8A8250b"
+}
+const helloWorld = () => {
+  console.log("Hello World!")
+  
+  async function main() {
+      const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl);
+      // const accountAPI = getSimpleAccount(
+      // provider,
+      // config.signingKey,
+      // config.entryPoint,
+      // config.simpleAccountFactory
+      // );
+      // const address = await accountAPI.getCounterFactualAddress();
+  
+      // console.log(`SimpleAccount address: ${address}`);
+  }
+  
+  main().catch((error) => {
+      console.error(error);
+      process.exit(1);
+  });
+}
 
 function App() {
   return (
